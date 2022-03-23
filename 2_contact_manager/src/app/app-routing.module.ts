aeaa8f0af9095f704e7e90d6f8e91d3f264e8052
lapinRagnar <lapinragnar@gmail.com>
2022-03-23 16:26:27 +0100

    generer - page-not-found et son routing

--- 2_contact_manager/src/app/app-routing.module.ts
+++ 2_contact_manager/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddContactComponent } from './components/add-contact/add-contact.component';
 import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
 import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ViewContactComponent } from './components/view-contact/view-contact.component';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'contact/add', component: AddContactComponent},
   {path: 'contact/edit/:contactId', component: EditContactComponent},
   {path: 'contact/view/:contactId', component: ViewContactComponent},
+  {path: '**', component: PageNotFoundComponent},
 
 ];
 
