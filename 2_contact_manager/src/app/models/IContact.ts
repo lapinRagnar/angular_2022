02cbae3c71a0f5683b233d9a16901dd7713e9a5c
lapinRagnar <lapinragnar@gmail.com>
2022-03-23 16:09:55 +0100

    ajout de model IContact et IGroup

---
+++ 2_contact_manager/src/app/models/IContact.ts
@@ -0,0 +1,11 @@
+export interface IContact {
+    id? : String
+    name: string
+    email : String
+    photo: string
+    mobile: string
+    company: string
+    title: string
+    groupId: string
+
+}
