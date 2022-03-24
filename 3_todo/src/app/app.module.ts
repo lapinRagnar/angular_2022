1dbf6c36ff3955927fd1e3a04660df350bd122a3
lapinRagnar <lapinragnar@gmail.com>
2022-03-24 12:39:03 +0100

    creer composant bouton et page not-found

--- 3_todo/src/app/app.module.ts
+++ 3_todo/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
