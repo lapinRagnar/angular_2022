f8a5f7caa1806e872d2e9e5525f64fc07b0aca7a
lapinRagnar <lapinragnar@gmail.com>
2022-03-24 20:15:47 +0100

    crud read detaile - Add a click event binding

--- tour_of_heroes/src/app/components/heroes/heroes.component.ts
+++ tour_of_heroes/src/app/components/heroes/heroes.component.ts
@@ -9,17 +9,17 @@ import { HEROES } from '../../models/mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'bitrobe'
-  }
-
   heroes = HEROES
+  selectedHero?: Hero
   
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero
   }
 
 }
