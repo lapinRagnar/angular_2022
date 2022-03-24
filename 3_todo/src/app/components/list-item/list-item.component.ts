5a686be950009d47e9256a5ccf9d8e2c7a107311
lapinRagnar <lapinragnar@gmail.com>
2022-03-24 17:25:01 +0100

    ne marche pas

--- 3_todo/src/app/components/list-item/list-item.component.ts
+++ 3_todo/src/app/components/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
+   
   listItem = {
     title: 'apprendre à coder',
     dueDate: "15/02/2023",
--- 3_todo/src/app/pages/home/home.component.ts
+++ 3_todo/src/app/pages/home/home.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  listItem: object = {
+    title: 'apprendre à coder',
+    dueDate: "15/02/2023",
+    completed: false,
+    favourite: false
+  }
+
   constructor() { }
 
   ngOnInit(): void {
