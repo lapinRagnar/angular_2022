3beb0b7d8a289c07cbf59b1341f965ab892469df
lapinRagnar <lapinragnar@gmail.com>
2022-03-25 07:05:04 +0100

    ajouter route /dashboard et le faire une route par defaut

--- tour_of_heroes/src/app/app-routing.module.ts
+++ tour_of_heroes/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
