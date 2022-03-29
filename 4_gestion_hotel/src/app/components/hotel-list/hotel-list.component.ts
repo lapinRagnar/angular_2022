7ae276460d9d07160e5f280ace939db879de0620
lapinRagnar <lapinragnar@gmail.com>
2022-03-29 16:22:27 +0200

    ajouter card pour lister les hotels

--- 4_gestion_hotel/src/app/components/hotel-list/hotel-list.component.ts
+++ 4_gestion_hotel/src/app/components/hotel-list/hotel-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelListComponent implements OnInit {
 
+  public title= "Liste d'hotel"
   constructor() { }
 
   ngOnInit(): void {
