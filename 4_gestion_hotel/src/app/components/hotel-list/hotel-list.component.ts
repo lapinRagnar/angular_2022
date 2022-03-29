122bb674484ebafbe6e36ee12597dee053ba4575
lapinRagnar <lapinragnar@gmail.com>
2022-03-30 00:42:58 +0200

    Comprendre les services et l'injection des dépendances - variables disponible partout

--- 4_gestion_hotel/src/app/components/hotel-list/hotel-list.component.ts
+++ 4_gestion_hotel/src/app/components/hotel-list/hotel-list.component.ts
@@ -1,3 +1,4 @@
+import { HotelListService } from './../../services/hotel-list.service';
 import { IHotel } from './../../interfaces/hotel';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,40 +11,7 @@ export class HotelListComponent implements OnInit {
 
   public title= "Liste d'hotel"
 
-  public hotels: IHotel[] = [
-    {
-          "hotelId": 1,
-          "hotelName": "Buea sweet life",
-          "description": "Belle vue au bord de la mer",
-          "price": 230.5,
-          "imageUrl": "assets/img/hotel-room.jpg",
-          "rating": 3.5
-    },
-    {
-          "hotelId": 2,
-          "hotelName": "Marakech",
-          "description": "Profitez de la vue sur les montagnes",
-          "price": 145.5,
-          "imageUrl": "assets/img/the-interior.jpg",
-          "rating": 5
-    },
-    {
-          "hotelId": 3,
-          "hotelName": "Abudja new look palace",
-          "description": "Séjour complet avec service de voitures",
-          "price": 120.12,
-          "imageUrl": "assets/img/indoors.jpg",
-          "rating": 4
-    },
-    {
-          "hotelId": 4,
-          "hotelName": "Cape town city",
-          "description": "Magnifique cadre pour votre séjour",
-          "price": 135.12,
-          "imageUrl": "assets/img/window.jpg",
-          "rating": 2.5
-    }
-  ]
+  public hotels: IHotel[] = []
 
   public showBadge: boolean = true
 
@@ -53,10 +21,11 @@ export class HotelListComponent implements OnInit {
 
   public receivedRating: string = ''
 
-  constructor() { }
+  constructor(public hotelListService: HotelListService) { }
 
   ngOnInit(): void {
 
+    this.hotels = this.hotelListService.getHotels()
     this.filteredHotels = this.hotels
     this.hotelFilter = ""
     
