b7505fe1a7a071095b4fdba19fde915498180f4a
lapinRagnar <lapinragnar@gmail.com>
2022-03-31 07:02:03 +0200

    Navigation avec le routerLink et router-outlet

--- 4_gestion_hotel/src/app/app-routing.module.ts
+++ 4_gestion_hotel/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
+import { HotelDetailComponent } from './components/hotel-list/hotel-detail/hotel-detail.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'hotels/:id', component: HotelDetailComponent},
+  {path: 'hotels', component: HotelListComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
