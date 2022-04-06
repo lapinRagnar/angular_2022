be28fbc0778b3bd74b16fae9f000e5d553a5bee6
lapinRagnar <lapinragnar@gmail.com>
2022-04-06 13:08:58 +0200

    ex - creer notre propre operateur - ne marche pas

--- 6_rxjs_et_angular/src/app/app.component.ts
+++ 6_rxjs_et_angular/src/app/app.component.ts
@@ -18,6 +18,22 @@ export class AppComponent implements OnInit, OnDestroy  {
 
   }
 
+  
+  double = (source: Observable<number>) => {
+    new Observable<number>((subscriber) => {
+      const subscription = source.subscribe({
+        next: (value) => subscriber.next(2 * value),
+        error: (err) => subscriber.error(err),
+        complete: () => subscriber.complete()
+      })
 
+      return () => {
+        subscription.unsubscribe()
+      }
+
+    })
+  }
+  
+  monObser = of(1, 2, 3).pipe(double).subscribe(console.log)
 
 }
