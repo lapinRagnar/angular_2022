af0ecd26474229f3a0aacf7a2ade659d8bb1f0f4
lapinRagnar <lapinragnar@gmail.com>
2022-04-06 10:54:32 +0200

    ex2 - avec avec la methode add de rxjs (la meilleurs methode)

--- 6_rxjs_et_angular/src/app/app.component.ts
+++ 6_rxjs_et_angular/src/app/app.component.ts
@@ -8,40 +8,23 @@ import { interval, Observable, Subscription } from 'rxjs';
 })
 export class AppComponent implements OnInit  {
 
-  private subscriptions: Subscription[] = []
+  private subscriptions: Subscription = new Subscription()
   
   ngOnInit(): void {
 
-    // const observer = {
-    //   next: (item: unknown) => console.log(`une boite arrive ${item} `),
-    //   error: (err: unknown) => console.log(`oups, il y a une erreur ${err} `),
-    //   complete: () => console.log('terminé, notre sequence est terminée les mecs! ')
-    // }
 
-    // const stream = new Observable(myObserver => {
-    //   myObserver.next('boite 1')
-    //   myObserver.next('boite 2')
-    //   myObserver.next('boite 3')
-    //   myObserver.complete()
-    // })
-
-    // const subscription = stream.subscribe(
-    //   item => console.log(`une boite arrive ${item}`),
-    //   err => console.log(`oups, il y a une erreur ${err}`),
-    //   () => console.log('mission accomplie!')            
-    // )
 
   }
 
   public start(): void {
 
-    this.subscriptions.push(interval(2000).subscribe(
+    this.subscriptions.add(interval(2000).subscribe(
       value => console.log("observable 1 --- ma valeur est: ", value),
       error => console.error(error),
       () => console.log('observable 1 --- terminé') 
     ))
 
-    this.subscriptions.push(interval(4000).subscribe(
+    this.subscriptions.add(interval(4000).subscribe(
       value => console.log("observable 2 --- ma valeur est: ", value),
       error => console.error(error),
       () => console.log('observable 2 --- terminé') 
@@ -49,10 +32,7 @@ export class AppComponent implements OnInit  {
   }
 
   public stop():void {
-    this.subscriptions.forEach(elem => {
-      elem.unsubscribe()
-    })
-    
+    this.subscriptions.unsubscribe()
   }
 
 }
