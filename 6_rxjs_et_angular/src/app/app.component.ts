efe58502898736f61135b54bf927f2fd806a6530
lapinRagnar <lapinragnar@gmail.com>
2022-04-06 14:30:11 +0200

    ex filter() - filtrer les valeur 0 et undefined

--- 6_rxjs_et_angular/src/app/app.component.ts
+++ 6_rxjs_et_angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Observable, Subscription, of, from, map, tap, take } from 'rxjs';
+import { interval, Observable, Subscription, of, from } from 'rxjs';
+import { map, tap, take, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -13,24 +14,14 @@ export class AppComponent implements OnInit, OnDestroy  {
   ngOnInit(): void {
 
 
-    from([1, 2, 3, 15, 16, 17, 18])
+    from([1, 2, 0, 3,undefined, 15, 16, 17, 18])
       .pipe(
         
-        tap(x => console.log('valeur originale sans modif : ',x)),
-        map((elem: number) => {
-          if (elem === 0) {
-            throw new Error('le nombre est zero')
-          }
-          return 2 * elem
-        } ),
-
-        map (item => item - 10 ),
-
-        take(2)
+       filter(element => element !== undefined && element !== 0)
 
       )
       .subscribe(
-      (item: number) => console.log(`ma valeur ${item}`),
+      (item: number | undefined) => console.log(`ma valeur ${item}`),
       (err: unknown) => console.error(err),
       () => console.log('terminé')
     )
