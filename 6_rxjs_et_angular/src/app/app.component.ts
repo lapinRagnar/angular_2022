47a9fd343cab86507c459898b82ba90b6df7162c
lapinRagnar <lapinragnar@gmail.com>
2022-04-06 10:33:05 +0200

    exemple - comment on crée et stoppe une observable

--- 6_rxjs_et_angular/src/app/app.component.ts
+++ 6_rxjs_et_angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +7,44 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit  {
+
+  private subscriptions: any
   
   ngOnInit(): void {
 
-    const observer = {
-      next: (item: unknown) => console.log(`une boite arrive ${item} `),
-      error: (err: unknown) => console.log(`oups, il y a une erreur ${err} `),
-      complete: () => console.log('terminé, notre sequence est terminée les mecs! ')
-    }
+    // const observer = {
+    //   next: (item: unknown) => console.log(`une boite arrive ${item} `),
+    //   error: (err: unknown) => console.log(`oups, il y a une erreur ${err} `),
+    //   complete: () => console.log('terminé, notre sequence est terminée les mecs! ')
+    // }
 
-    const stream = new Observable(myObserver => {
-      myObserver.next('boite 1')
-      myObserver.next('boite 2')
-      myObserver.next('boite 3')
-      myObserver.complete()
-    })
+    // const stream = new Observable(myObserver => {
+    //   myObserver.next('boite 1')
+    //   myObserver.next('boite 2')
+    //   myObserver.next('boite 3')
+    //   myObserver.complete()
+    // })
 
-    const subscription = stream.subscribe(
-      item => console.log(`une boite arrive ${item}`),
-      err => console.log(`oups, il y a une erreur ${err}`),
-      () => console.log('mission accomplie!')            
+    // const subscription = stream.subscribe(
+    //   item => console.log(`une boite arrive ${item}`),
+    //   err => console.log(`oups, il y a une erreur ${err}`),
+    //   () => console.log('mission accomplie!')            
+    // )
+
+  }
+
+  public start(): void {
+    this.subscriptions = interval(2000).subscribe(
+      value => console.log("ma valeur est: ", value),
+      error => console.error(error),
+      () => console.log('terminé') 
     )
+  }
 
+  public stop():void {
+    this.subscriptions.unsubscribe()
+    console.log("la séquence est terminé!, bye!");
+    
   }
 
 }
