e9a9e091c2200bdb8fa64911de3b2d9431b10aad
lapinRagnar <lapinragnar@gmail.com>
2022-04-10 05:55:32 +0200

    mettre la route dans authModule

--- mediumclone-angular/src/app/auth/auth.module.ts
+++ mediumclone-angular/src/app/auth/auth.module.ts
@@ -1,7 +1,17 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RegisterComponent } from "../components/register/register.component";
+
+const routes = [
+    {path: 'register', component: RegisterComponent}
+]
 
 @NgModule({
-    imports: [CommonModule]
+    imports: [
+        CommonModule, 
+        RouterModule.forChild(routes)
+    ],
+    declarations: [RegisterComponent]
 })
 export class AuthModule {}
