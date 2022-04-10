fac58ad202e18af34bd3d6e778a4f3e7176386a8
lapinRagnar <lapinragnar@gmail.com>
2022-04-10 10:26:31 +0200

    3 - installer redux devtools - npm install @ngrx/store-devtools --save -  configuration dans app.module.ts

--- mediumclone-angular/src/app/app.module.ts
+++ mediumclone-angular/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { environment } from './../../../6_rxjs_et_angular/src/environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +17,11 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,                               // Retains last 25 states
+      logOnly: environment.production,          // Restrict extension to log-only mode
+      autoPause: true                           // Pauses recording actions and state changes when the extension window is not open
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
