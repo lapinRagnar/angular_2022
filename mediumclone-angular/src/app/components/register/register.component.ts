debd351246570dcaa0475548253acedc03d17b6c
lapinRagnar <lapinragnar@gmail.com>
2022-04-10 06:16:29 +0200

    creer register layout

--- mediumclone-angular/src/app/components/register/register.component.ts
+++ mediumclone-angular/src/app/components/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'mc-register',
     templateUrl: './register.component.html',
-    styleUrls: ['./register.component.css']
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
 
