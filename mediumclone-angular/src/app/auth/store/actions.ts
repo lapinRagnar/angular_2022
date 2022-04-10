8907131817a1b726cc2260a833165c71dddfe6dc
lapinRagnar <lapinragnar@gmail.com>
2022-04-10 10:51:02 +0200

    4 - créer auth interface :    creer currentUser.interface.ts    registerRequest.interface    et typer dans actions

--- mediumclone-angular/src/app/auth/store/actions.ts
+++ mediumclone-angular/src/app/auth/store/actions.ts
@@ -1,7 +1,8 @@
+import { RegisterRequestInterface } from './../types/registerRequest.interface';
 import { ActionTypes } from './actionTypes';
 import { createAction, props } from "@ngrx/store";
 
 export const registerAction = createAction(
     ActionTypes.REGISTER,
-    props<{username: string; email:string; password: string}>()
+    props<{request: RegisterRequestInterface}>()
 )
---
+++ mediumclone-angular/src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,10 @@
+export interface CurrentUserInterface {
+    id: number
+    email: string
+    created: string
+    updateAt: string
+    username: string
+    bio: string | null
+    image: string | null
+    token: string
+}
