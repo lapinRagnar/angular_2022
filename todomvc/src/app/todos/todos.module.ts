732608116239d8ef9bf9a682314e33e48f397991
lapinRagnar <lapinragnar@gmail.com>
2022-04-12 11:15:43 +0200

    4 - ajouter header component: => créer dossier et fichier components/header/header.component.html et components/header/header.component.html => ecrire le html du header => ecrire le ts du header => enregistrer dans le module HeaderComponent dans la declarations => mettre à jour todos.component.html en ajoutant le composant header <app-todos-header></app-todos-header> => supprimer le titre dans app.component.html

--- todomvc/src/app/todos/todos.module.ts
+++ todomvc/src/app/todos/todos.module.ts
@@ -2,13 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { TodosComponent } from 'src/app/todos/components/todos.component';
+import { HeaderComponent } from 'src/app/todos/components/header/header.component';
 
 const routes: Routes = [
     {path: '', component: TodosComponent}
 ]
 
 @NgModule({
-    declarations: [TodosComponent],
+    declarations: [TodosComponent, HeaderComponent],
     imports: [RouterModule.forChild(routes)]
 })
 export class TodosModule {
