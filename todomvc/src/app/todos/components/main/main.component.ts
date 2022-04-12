107be494bcd7530bcc6c8ec634f6ac74ad8dc5bf
lapinRagnar <lapinragnar@gmail.com>
2022-04-12 16:16:09 +0200

    7 - rendering todos - creer la composant main : => on cree un dossier et fichier main/main.component.html et main/main.component.ts => ecrire le code du composant main => ajouter MainComponent dans le module todos.module.ts
    =>  ajouter le composant dans todos.component.html: <app-todos-main></app-todos-main>
    
    et la on peut voir notre composant au dessous de l'input

---
+++ todomvc/src/app/todos/components/main/main.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-todos-main',
+    templateUrl: './main.component.html'
+})
+export class MainComponent {
+
+}
--- todomvc/src/app/todos/todos.module.ts
+++ todomvc/src/app/todos/todos.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { TodosComponent } from 'src/app/todos/components/todos.component';
 import { HeaderComponent } from 'src/app/todos/components/header/header.component';
 import { TodosService } from 'src/app/todos/services/todos.service';
+import { MainComponent } from 'src/app/todos/components/main/main.component';
 
 const routes: Routes = [
     {path: '', component: TodosComponent}
 ]
 
 @NgModule({
-    declarations: [TodosComponent, HeaderComponent],
+    declarations: [TodosComponent, HeaderComponent, MainComponent],
     imports: [RouterModule.forChild(routes)],
     providers: [TodosService]
 })
