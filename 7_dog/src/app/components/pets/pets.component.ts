1bc103eeb8767077a80fc2e2c864c93c8c4f1524
lapinRagnar <lapinragnar@gmail.com>
2022-04-13 15:56:18 +0200

    4 - afficher les fake-data dans le html => declarer une variable pets dans les pets ts => dans le pets html, boubler avec *ngFor pour afficher le tableau des fake-data

--- 7_dog/src/app/components/pets/pets.component.ts
+++ 7_dog/src/app/components/pets/pets.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FakeData } from 'src/app/fake-data/fake-data';
+import { PetInterface } from 'src/app/interfaces/pet.interface';
+
 @Component({
   selector: 'app-pets',
   templateUrl: './pets.component.html',
   styleUrls: ['./pets.component.css']
 })
 export class PetsComponent implements OnInit {
+
+  pets: PetInterface[] = FakeData
 
   constructor() { }
 
