686a925d30534af9e65aeb27951c1e4d87f4453c
lapinRagnar <lapinragnar@gmail.com>
2022-04-13 15:14:13 +0200

    modifier le bouton - changer l'icone

--- 7_dog/src/app/app.module.ts
+++ 7_dog/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PetComponent } from './components/pet/pet.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {MatButtonModule} from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
