f1580c56caa8b6df1c3cc4d496b118c7874230aa
lapinRagnar <lapinragnar@gmail.com>
2022-04-13 13:56:58 +0200

    1 - creer component pet et lier dans le html > ng g c components/pet
    et installation de aangular material

--- 7_dog/src/app/app.module.ts
+++ 7_dog/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetComponent } from './components/pet/pet.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
