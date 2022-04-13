4c28ba9ebc83b3536ca89ea18578c0c6aab2bb55
lapinRagnar <lapinragnar@gmail.com>
2022-04-13 09:30:26 +0200

    afficher a gauche du footer le nombre de taches

--- todomvc/src/app/todos/footer/footer.component.ts
+++ todomvc/src/app/todos/footer/footer.component.ts
@@ -14,8 +14,19 @@ import { TodosService } from 'src/app/todos/services/todos.service';
 export class FooterComponent {
 
     noTodosClass$: Observable<boolean>
+    activeCount$: Observable<number>
+    itemsLeftText$: Observable<string>
+
 
     constructor(private todoService: TodosService){
+
+        this.activeCount$ = this.todoService.todos$.pipe(
+            map((todos) => todos.filter(todo => !todo.isCompleted).length)
+        )
+
+        this.itemsLeftText$ = this.activeCount$.pipe(
+            map((activeCount) => `item${activeCount !== 1 ? 's' : ''} left `)
+        )
         
         this.noTodosClass$ = this.todoService.todos$.pipe(
             map((todos => todos.length === 0))
