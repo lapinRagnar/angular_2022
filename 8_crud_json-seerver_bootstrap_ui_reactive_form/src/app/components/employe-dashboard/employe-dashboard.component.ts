4b5cc4f43d7acead080f8c6896c690214123cd9f
lapinRagnar <lapinragnar@gmail.com>
2022-04-17 12:54:29 +0200

    3 - composant employeeDashboard => ng g c components/employeeDashboard

---
+++ 8_crud_json-seerver_bootstrap_ui_reactive_form/src/app/components/employe-dashboard/employe-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employe-dashboard',
+  templateUrl: './employe-dashboard.component.html',
+  styleUrls: ['./employe-dashboard.component.css']
+})
+export class EmployeDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
