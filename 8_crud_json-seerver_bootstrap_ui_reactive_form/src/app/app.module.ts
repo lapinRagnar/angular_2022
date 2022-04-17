d95d8e277ac2748d628c74a959b6c8faf007919f
lapinRagnar <lapinragnar@gmail.com>
2022-04-17 15:24:08 +0200

    d - creer component contenu ng g c components/employe-dashboard/contenu

--- 8_crud_json-seerver_bootstrap_ui_reactive_form/src/app/app.module.ts
+++ 8_crud_json-seerver_bootstrap_ui_reactive_form/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeDashboardComponent } from './components/employe-dashboard/employe-dashboard.component';
 import { NavbarComponent } from './components/employe-dashboard/navbar/navbar.component';
+import { ContenuComponent } from './components/employe-dashboard/contenu/contenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeDashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContenuComponent
   ],
   imports: [
     BrowserModule,
