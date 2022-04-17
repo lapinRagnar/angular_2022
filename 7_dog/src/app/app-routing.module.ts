147b3add72837868fe2eb438490e64c7323b47e0
lapinRagnar <lapinragnar@gmail.com>
2022-04-17 12:22:00 +0200

    css styling

--- 7_dog/src/app/app-routing.module.ts
+++ 7_dog/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { AddPetComponent } from './components/add-pet/add-pet.component';
 
 import { PetsComponent } from './components/pets/pets.component';
 
 const routes: Routes = [
   { path: '', component: PetsComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'ajouter-pet', component: AddPetComponent}
 ];
 
 @NgModule({
