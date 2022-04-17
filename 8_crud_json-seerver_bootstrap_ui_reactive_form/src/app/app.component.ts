8c45ca2501fb330bc6959f53c4536db1b9f1ed93
lapinRagnar <lapinragnar@gmail.com>
2022-04-17 12:26:49 +0200

    initial commit

---
+++ 8_crud_json-seerver_bootstrap_ui_reactive_form/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '8_crud_json-seerver_bootstrap_ui_reactive_form';
+}
