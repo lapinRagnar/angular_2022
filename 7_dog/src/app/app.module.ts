92868010bc38f77c7530880cb2d56c21575d4df1
lapinRagnar <lapinragnar@gmail.com>
2022-04-17 07:17:26 +0200

    10 - creer un formulaire => on va creer d'abord un composant  add-pet => ajouter ce composant dans pets component html

--- 7_dog/src/app/app.module.ts
+++ 7_dog/src/app/app.module.ts
@@ -11,12 +11,16 @@ import {MatIconModule} from '@angular/material/icon';
 import { PetsComponent } from './components/pets/pets.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { HttpClientModule } from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { AddPetComponent } from './components/add-pet/add-pet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PetComponent,
     PetsComponent,
+    AddPetComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +30,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatIconModule,
     MatGridListModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
