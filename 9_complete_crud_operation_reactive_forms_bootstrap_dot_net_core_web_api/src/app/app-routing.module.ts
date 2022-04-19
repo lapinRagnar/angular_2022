054b08ab2abe90e94847a3c4fe8b81ee2e57f8b1
lapinRagnar <lapinragnar@gmail.com>
2022-04-19 20:17:48 +0200

    2 - routing

--- 9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api/src/app/app-routing.module.ts
+++ 9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { EmpaddComponent } from './empadd/empadd.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmplistingComponent } from './emplisting/emplisting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'employee', component: EmployeeComponent, children: [
+    {path: '', component: EmplistingComponent},
+    {path: 'create', component: EmpaddComponent},
+    {path: 'edit/:id', component: EmpaddComponent},
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
