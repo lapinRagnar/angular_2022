1b6456418821890d246e7fa7feb5f089f89634a5
lapinRagnar <lapinragnar@gmail.com>
2022-04-19 19:43:39 +0200

    initial commit - installation de bootstrap

---
+++ 9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api';
+}
