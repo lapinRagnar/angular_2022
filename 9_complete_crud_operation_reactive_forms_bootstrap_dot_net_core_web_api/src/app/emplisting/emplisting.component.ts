97daec13cbcc1d4331c31392e3d292c1016308fa
lapinRagnar <lapinragnar@gmail.com>
2022-04-20 22:39:15 +0200

    appel api pour afficher tous les employee => on cree une fonction loadAllEmployee() dans le service employee qui retourne tous les employee => on cree une fonction employeeByCode() dans le service employe => on cree removeEmployeeByCode() => on cree saveEmployee()
    dans EmplistingComponent ts:
    => on cree un fonction loadAll()
    => on met this.loadAll() dans le constructeur
    
    dans EmplistingComponent html:
    => on boucle sur empoyeeData, et on affiche:
    <tr *ngFor="let item of employeeData" >
        <td> {{item.code}} </td>
        <td> {{item.name}} </td>
        <td> {{item.email}} </td>
        <td> {{ item.phone }} </td>
        <td> {{item.designation}} </td>
    </tr>
    
    => on ajoute le lien pour edit et delete:
    routerLink="edit/{{item.code}}"
    
    => sur le bouton delete on ajoute :
    (click)="delete(item.code)
    
    => on cree la fonction delete() dans le ts

--- 9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api/src/app/emplisting/emplisting.component.ts
+++ 9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api/src/app/emplisting/emplisting.component.ts
@@ -1,3 +1,4 @@
+import { EmployeeService } from './../services/employee.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmplistingComponent implements OnInit {
 
-  constructor() { }
+  employeeData: any
+
+  constructor(private employeeService: EmployeeService) { 
+    this.loadAll()
+  }
 
   ngOnInit(): void {
+  }
+
+  loadAll() {
+    this.employeeService.loadAllEmployee().subscribe(result => {
+      this.employeeData = result
+    })
+  }
+
+  delete(code: any) {
+    
   }
 
 }
--- 9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api/src/app/services/employee.service.ts
+++ 9_complete_crud_operation_reactive_forms_bootstrap_dot_net_core_web_api/src/app/services/employee.service.ts
@@ -7,10 +7,27 @@ import { Injectable } from '@angular/core';
 export class EmployeeService {
 
   apiurl_des = "http://localhost:9000/designation"
+  apiurl = "http://localhost:9000/employee"
   
   constructor(private http: HttpClient) { }
 
   loadDesignation() {
     return this.http.get(this.apiurl_des)
   }
+
+  loadAllEmployee() {
+    return this.http.get(this.apiurl)
+  }
+
+  employeeByCode(code: any) {
+    return this.http.get(this.apiurl + '/' + code)
+  }
+  removeEmployeeByCode(code: any) {
+    return this.http.delete(this.apiurl + '/' + code)
+  }
+
+  saveEmployee(inputData: any) {
+    return this.http.post(this.apiurl, inputData)
+  }
+
 }
