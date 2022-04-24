4f5778dea4291367f6ff8854cd9bc6ed516e5f7b
lapinRagnar <lapinragnar@gmail.com>
2022-04-24 12:10:54 +0200

    ajouter le routing

--- 10_crud_student_management_with_jason-server/src/app/app-routing.module.ts
+++ 10_crud_student_management_with_jason-server/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { ListStudentComponent } from './components/list-student/list-student.component';
+import { RegistrationStudentComponent } from './components/registration-student/registration-student.component';
+import { LoginStudentComponent } from './components/login-student/login-student.component';
+
+const routes: Routes = [
+  {path: 'add', component: AddStudentComponent},
+  {path: 'edit', component: EditStudentComponent},
+  {path: 'list', component: ListStudentComponent},
+  {path: 'login', component: LoginStudentComponent},
+  {path: 'register', component: RegistrationStudentComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
