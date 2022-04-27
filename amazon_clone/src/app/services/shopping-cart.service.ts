0e465fdc6a2f877fa32e86407a533af93d39543f
lapinRagnar <lapinragnar@gmail.com>
2022-04-27 17:09:31 +0200

    on cree un tableau shoppingCartItems: any = [] et faire un push quand l'utilisateur clique sur ajouter un produit.

--- amazon_clone/src/app/services/shopping-cart.service.ts
+++ amazon_clone/src/app/services/shopping-cart.service.ts
@@ -5,11 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class ShoppingCartService {
 
+  shoppingCartItems: any = []
+
   constructor() { }
 
   addProduct = (product: any) => {
     console.log("ajouter au localstorage", product);
-    localStorage.setItem('shopping_cart', JSON.stringify(product))
+    let items = this.getShoppingCartItems()
+
+    if (items) {
+      items.push(product)
+      localStorage.setItem('shopping_cart', JSON.stringify(items))
+    } else {
+      this.shoppingCartItems.push(product)
+      localStorage.setItem('shopping_cart', JSON.stringify(this.shoppingCartItems))
+    }
+  }
+
+  getShoppingCartItems() {
+    let items: any = localStorage.getItem('shopping_cart')
+    return JSON.parse(items)
   }
 
 }
