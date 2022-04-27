ee5b9da8145ce8186efeaca775f9e5dede2d316c
lapinRagnar <lapinragnar@gmail.com>
2022-04-27 12:12:36 +0200

    cree composant home, footer, checkout, login ng g c pages/home ng g c components/footer ng g c pages/login ng g c pages/checkout

--- amazon_clone/src/app/app.module.ts
+++ amazon_clone/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { mdiMenu } from '@mdi/js';
 import {MatIconModule} from '@angular/material/icon';
-import { HeaderComponent } from './components/header/header.component'
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { LoginComponent } from './pages/login/login.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CheckoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
