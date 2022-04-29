4eee8a5c545fde76deb124b3cf94f3c2558ae997
lapinRagnar <lapinragnar@gmail.com>
2022-04-29 23:35:40 +0200

    7 -ActivatedRoute - coder le html la pager movie avec id et type
    => dans le html de movie on code le html
    
    => dans le ts de movie :
          - on declare 2 variables :
             type: any = ''
             id: string = ''
          - on injecte ActivatedRoute :
            constructor(private activatedRoute: ActivatedRoute) { }
    
           - on assigne pendant le demarrage de l'appli au deux variables type et id les valeurs de parametre de l'url :
    
             ngOnInit(): void {
        this.type = this.activatedRoute.snapshot.params['type']
        this.id = this.activatedRoute.snapshot.params['id']
      }
    
    => encore dans le ts du movie, on cree cree une fonction pour recuperer le movie qui a l'id dans le parametre de l'url :
    
      movies: any
    
      constructor(
        private activatedRoute: ActivatedRoute,
        private http: HttpClient
      ) { }
    
        ngOnInit(): void {
        this.type = this.activatedRoute.snapshot.params['type']
        this.id = this.activatedRoute.snapshot.params['id']
    
        if (this.type === 'trending') {
          this.url = 'http://localhost:4200/assets/data/trending-movies.json'
        }
        if (this.type === 'theatre') {
          this.url = 'http://localhost:4200/assets/data/theatre-movies.json'
        }
        if (this.type === 'popular') {
          this.url = 'http://localhost:4200/assets/data/popular-movies.json'
        }
    
        this.getMovie()
      }
    
      getMovie() {
        this.http.get(this.url).subscribe((movies) => {
          this.movies = movies
          let index = this.movies.findIndex((movie: {id: string}) =>{
            movie.id == this.id
          })
          if (index > - 1) {
            this.movie = this.movies[index]
          }
        })
      }
    
    nb : ActivatedRoute permet de recuperer les parametre de l'url

--- zero_to_hero/src/app/components/movie/movie.component.ts
+++ zero_to_hero/src/app/components/movie/movie.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
-  constructor() { }
+  type: any = ''
+  id: string = ''
+
+  url: string = ''
+
+  movies: any
+  movie: any
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient  
+  ) { }
 
   ngOnInit(): void {
+    this.type = this.activatedRoute.snapshot.params['type']
+    this.id = this.activatedRoute.snapshot.params['id']
+
+    if (this.type === 'trending') {
+      
+      this.url = 'http://localhost:4200/assets/data/trending-movies.json'
+    }
+    if (this.type === 'theatre') {
+
+      this.url = 'http://localhost:4200/assets/data/theatre-movies.json'
+    }
+    if (this.type === 'popular') {
+
+      this.url = 'http://localhost:4200/assets/data/popular-movies.json'
+    }
+    
+    this.getMovie()
+  }
+
+  getMovie() {
+    this.http.get(this.url).subscribe((movies) => {
+      
+      this.movies = movies
+
+      let index = this.movies.findIndex((movie: {id: string}) => movie.id == this.id)
+      if (index > - 1) {
+        this.movie = this.movies[index]
+        console.log("dans getMovie()", this.movie);
+        return this.movie
+      }
+    })
   }
 
 }
