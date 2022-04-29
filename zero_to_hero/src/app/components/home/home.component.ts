668da6b51d11d53d788590d6b65fba389b997385
lapinRagnar <lapinragnar@gmail.com>
2022-04-29 17:31:53 +0200

    on cree la section html du theatre movie dans home html
    => on ajoute dans la ts home la varible :
    theatreMovies : any
    
    pour mettre les données de la base de données
    
    nb: faire le meme manip comme precedement dans la section popular movie.
    
    on copie colle et on modifie la fonction dans le ts home :  getTheatreMovies()
    
    ensuite, on l'ajoute dans le ngOnInit() : this.getTheatreMovies()

--- zero_to_hero/src/app/components/home/home.component.ts
+++ zero_to_hero/src/app/components/home/home.component.ts
@@ -8,20 +8,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  url = "http://localhost:4200/assets/data/trending-movies.json"
+  urlTrendingMovies = "http://localhost:4200/assets/data/trending-movies.json"
+  urlTheatreMovies = "http://localhost:4200/assets/data/theatre-movies.json"
+  
   trendingMovies: any
   
+  theatreMovies: any
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getTrendingMovies()
+    this.getTheatreMovies()
   }
 
   getTrendingMovies() {
-    this.http.get(this.url).subscribe((movies) => {
+    this.http.get(this.urlTrendingMovies).subscribe((movies) => {
       this.trendingMovies = movies
       console.log(this.trendingMovies)
+    })
+  }
+
+  getTheatreMovies() {
+    this.http.get(this.urlTheatreMovies).subscribe((movies) => {
+      this.theatreMovies = movies
     })
   }
 
