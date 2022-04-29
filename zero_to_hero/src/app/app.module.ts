7a7909598c4be0f376a0a94afd3b4c28122a08c7
lapinRagnar <lapinragnar@gmail.com>
2022-04-29 11:18:13 +0200

    on cree les composant login, home, header, movie :
    > ng g c components/login
    > ng g c components/home
    > ng g c components/header
    > ng g c components/movie

--- zero_to_hero/src/app/app.module.ts
+++ zero_to_hero/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
