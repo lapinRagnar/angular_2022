bc931ece98c6debf076a5212a56cea7d5868decc
lapinRagnar <lapinragnar@gmail.com>
2022-05-01 19:57:27 +0200

    recuperer un pokemon de la mock-data et l'afficher en console lors de l'initialisation du composant (life cycle)

--- pokemon/src/app/app.component.ts
+++ pokemon/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemonType';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pokemon';
+export class AppComponent implements OnInit {
+  
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit() {
+    console.table(this.pokemonList)
+    this.selectPokemon(this.pokemonList[0])
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`vous avez selectionner ${pokemon.name} `)
+  }
+
 }
