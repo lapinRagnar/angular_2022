c80aabc8d9f64150ddd73cb62831342eb852b7eb
lapinRagnar <lapinragnar@gmail.com>
2022-05-04 18:44:38 +0200

    13 - http request - appel reseau - api
    mettre en place le client http HttpClientModule, les etapes :
    
    => dans le app module, on ajoute dans les imports :
    HttpClientModule
    
    => simuler une bdd avec json-server :
    j'ai ajouté un fichier db.json dans pokemon/db.json

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,6 +16,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
   imports: [
     BrowserModule,
     PokemonModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule
   ],
