37faebc6ff63551cf5127b44d211c1dd9d68a6af
lapinRagnar <lapinragnar@gmail.com>
2022-05-06 08:56:01 +0200

    programmation reactive - Subject - implementer les fonctions dans search-pokemon ts
    https://www.youtube.com/watch?v=DTIYVffhJuU

--- pokemon/src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ pokemon/src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,4 +1,8 @@
+import { PokemonService } from 'src/app/pokemon/pokemon.service';
+import { Observable, Subject, debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs';
+import { Pokemon } from 'src/app/pokemon/pokemonType';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-pokemon',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPokemonComponent implements OnInit {
 
-  constructor() { }
+  // {...'ab'......'abz'.....'ab'.....'abc'....}
+  searchTerms = new Subject<string>()
+
+  // {...pokemonList('ab')....pokemonList('abz').........}
+  pokemons$: Observable<Pokemon[]>
+
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService
+  ) { }
 
   ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term) => this.pokemonService.searchPokemonList(term))
+    )
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term)
+  }
+
+  goToDetail(pokemon: Pokemon) {
+    const link = ['/pokemon', pokemon.id]
+    this.router.navigate(link)
   }
 
 }
